cd17e90685c5e09d9dff0e780a32f4f650a38de8
Attama Pascal <Attamapascalpedro@gmail.com>
2023-03-02 21:09:41 +0000

    Error fixing

---
+++ frontend-react-js/src/components/ActivityActionShare.js
@@ -0,0 +1,9 @@
+import './Search.css';
+
+export default function ReplyForm(props) {
+  return (
+    <div className='search_field'>
+      <input type='text' placeholder='Search Cruddur' />
+    </div>
+  );
+}

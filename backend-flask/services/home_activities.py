5d28823a89f17608bd2ffbba77fc13d52f0cc464
Attama Pascal <Attamapascalpedro@gmail.com>
2023-03-02 21:36:38 +0000

    Updates

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -5,7 +5,7 @@ class HomeActivities:
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Attama Pascal',
-      'message': 'Cloud is fun!',
+      'message': 'Cloud computing is fun-filled!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
